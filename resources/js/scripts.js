70303b0d002c3f74e68cee337ee14e49407d2746
John Quarmor <johnequarmor@gmail.com>
2020-07-05 06:49:17 +0000

    1st functional iteration. Further edits advised.

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,22 +1,47 @@
-const input = document.getElementById("input");
-input.setAttribute("placeholder", 0); //I'm Simply testing the setAttribute() method.
+const dataAmount = document.getElementById("input");
+//Assigning a placeholder numerical value of "0" to the number input HTML element.
+dataAmount.setAttribute("placeholder", 0);
 
-const selectedMonth = document.querySelector("");
-const monthsArray = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]; //days of each month of the year.
-// let month = monthsArray[];
-const button = document.getElementById("submit-btn");
+let monthSelect = document.getElementById("month-select");
+
+const button = document.getElementById("calculate-btn");
 const output = document.getElementById("result-output");
 
+//(Below) An event listener to listen for the calculate button click to run calculateBudget() function.
 button.addEventListener("click", calculateBudget);
 
 //Test logs:
 // console.log(month[0]);
-// console.log(input.value);
-console.log(selectedMonth);
+// console.log(dataAmount.value);
+// console.log(monthSelect);
 
-function calculateBudget() {
-	let dataPerDay = input.value / month;
-	return dataPerDay;
+function calculateBudget(e) {
+	e.preventDefault(); //Prevents the default action of the submit button.
+	const monthDaysArray = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+	//(Above) An array containing the days of each month respectively in ascending order.
+	let selectedMonth = monthSelect.options[monthSelect.selectedIndex].value;
+	// (Above) returns the value (a number) associated to the month selected.
+	let month = monthDaysArray[selectedMonth];
+	//(Below) Passed the selected month's value (a number) to assign the selected month's days to the variable month.
+
+	if (
+		dataAmount.value == "" &&
+		selectedMonth === "#" &&
+		dataAmount.value == 0
+	) {
+		console.log("Please enter your data amount and select a month");
+	} else if (selectedMonth === "#") {
+		console.log("Please select the month you wish to budget for");
+	} else if (dataAmount.value == "" || dataAmount.value == 0) {
+		console.log("Please enter your data amount");
+	} else {
+		let dataPerDay = dataAmount.value / month;
+
+		output.innerHTML = `${dataPerDay} <b>Megabytes</b> (per day)`;
+
+		//Test logs:
+		// console.log("data amount: " + dataAmount.value);
+		// console.log("month's days: " + month);
+		// console.log("Data per day: " + dataPerDay + "MB");
+	}
 }
-console.log(calculateBudget());
-// function outputResult() {}
